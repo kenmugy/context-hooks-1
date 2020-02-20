c9d7942a85bd7a203f37f888c84ed5b42dfcf4a8
kenmugy <42069371+kenmugy@users.noreply.github.com>
2020-02-20 10:32:21 +0300

    consumed state with static contextType and added some colors

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,16 +1,19 @@
 import React, { Component } from 'react';
+import { ThemeContext } from '../contexts/ThemeContext';
 
 class BookList extends Component {
-  state = {};
+  static contextType = ThemeContext;
   render() {
+    const { isLight, dark, light } = this.context;
+    const theme = isLight ? light : dark;
     return (
-      <div className=''>
+      <div style={{ background: theme.bg, color: theme.color }}>
         <h3>book lists</h3>
-        <ul className="books">
-          <li>Things fall Apart</li>
-          <li>Reciepe for disaster</li>
-          <li>Among the fools</li>
-          <li>Empre strikes back</li>
+        <ul className='books'>
+          <li style={{ background: theme.ui }}>Things fall Apart</li>
+          <li style={{ background: theme.ui }}>Reciepe for disaster</li>
+          <li style={{ background: theme.ui }}>Among the fools</li>
+          <li style={{ background: theme.ui }}>Empre strikes back</li>
         </ul>
       </div>
     );
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,13 +1,16 @@
 import React, { Component } from 'react';
+import { ThemeContext } from '../contexts/ThemeContext';
 
 class Navbar extends Component {
-  state = {};
+  static contextType = ThemeContext;
   render() {
+    const { isLight, dark, light } = this.context;
+    const theme = isLight ? light : dark
     return (
-      <div className=''>
+      <div style={{background: theme.ui, color: theme.color}}>
         <h2>Context APP</h2>
-        <ul className="nav">
-          <li>Home</li>
+        <ul className='nav'>
+          <li >Home</li>
           <li>About</li>
           <li>More</li>
         </ul>
